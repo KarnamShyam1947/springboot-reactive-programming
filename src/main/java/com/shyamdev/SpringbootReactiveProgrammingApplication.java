4d4f577e84dbb586a54eba12aebae2c87edfd088
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2025-09-19 21:42:54 +0530

    project structure setup

---
+++ src/main/java/com/shyamdev/SpringbootReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.shyamdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactiveProgrammingApplication.class, args);
+	}
+
+}
